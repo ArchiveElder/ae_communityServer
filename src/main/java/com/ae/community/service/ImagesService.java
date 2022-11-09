bbb3b4c2cdc117fe09f34f709e6488b12bb5e318
Vector1331 <tpgus4796@gmail.com>
2022-11-09 11:48:20 +0900

    [Feat] Posting, Image, USer service, repository 코드 추가

---
+++ src/main/java/com/ae/community/service/ImagesService.java
@@ -0,0 +1,52 @@
+package com.ae.community.service;
+
+import com.ae.community.aws.S3Uploader;
+import com.ae.community.domain.Images;
+import com.ae.community.repository.ImagesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ImagesService {
+
+    private final S3Uploader s3Uploader;
+    private final ImagesRepository imagesRepository;
+
+    public Images save(Images image) {return  imagesRepository.save(image); }
+
+    public Images create(Long postIdx, String imgUrl, int imgRank) {
+        Images images = new Images();
+        images.setPostIdx(postIdx);
+        images.setImgUrl(imgUrl);
+        images.setImgRank(imgRank);
+        return images;
+    }
+
+
+    public void deleteByPostIdx(Long postIdx) {
+        imagesRepository.deleteByPostIdx(postIdx);
+    }
+
+    public Optional<Images> findByIdx(Long imageIdx) { return imagesRepository.findById(imageIdx);
+    }
+
+    public void deleteImage(Long imageIdx) { imagesRepository.deleteById(imageIdx);    }
+
+    public void update(Images targetImage, String updateImgUrl, int updateImgRank) {
+        targetImage.setImgUrl(updateImgUrl);
+        targetImage.setImgRank(updateImgRank);
+
+        imagesRepository.save(targetImage);
+    }
+
+    public List<Images> findByPostIdx(Long postIdx) {
+        return imagesRepository.findByPostIdx(postIdx);
+    }
+
+}
