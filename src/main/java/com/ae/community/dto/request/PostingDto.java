4bcdc461ff17779411e0f1428eb1b3ebcf052866
Vector1331 <tpgus4796@gmail.com>
2022-11-09 11:50:45 +0900

    [Feat] 게시글 작성 기능 추가 (김세현)

---
+++ src/main/java/com/ae/community/dto/request/PostingDto.java
@@ -0,0 +1,12 @@
+package com.ae.community.dto.request;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+@Data
+public class PostingDto {
+    @ApiModelProperty(value = "게시글 제목")
+    private String title;
+    @ApiModelProperty(value = "게시글 내용")
+    private String content;
+}
