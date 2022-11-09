39247418a8b774deec0c435ddf4a7a85e5748c01
rosesua318 <rosesua@duksung.ac.kr>
2022-11-09 21:28:29 +0900

    [Test] 스크랩 등록, 스크랩 삭제 테스트 코드 추가 (김민서)

---
+++ src/test/java/com/ae/community/service/ScrapServiceTest.java
@@ -0,0 +1,76 @@
+package com.ae.community.service;
+
+import com.ae.community.domain.CommunityUser;
+import com.ae.community.domain.Posting;
+import com.ae.community.domain.Scrap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@WebAppConfiguration
+@SpringBootTest
+@Transactional
+public class ScrapServiceTest {
+    @Autowired
+    CommunityUserService userService;
+
+    @Autowired
+    PostingService postingService;
+
+    @Autowired
+    ScrapService scrapService;
+
+    @Test
+    void 스크랩생성() {
+        // given
+        CommunityUser user = new CommunityUser();
+        String nickname = "dr.김";
+        user.setNickname(nickname);
+        user.setIdx(13L);
+        CommunityUser userT = userService.save(user);
+
+        Posting post = new Posting();
+        String content = "안녕하세요";
+        String title = "제목";
+        Long userIdx = userT.getIdx();
+        Posting create_post = postingService.create(userIdx, content, title);
+        Posting save_post = postingService.save(create_post);
+
+        Scrap scrap = Scrap.createScrap(userT.getIdx(), save_post.getIdx());
+
+        // when
+        Long id = scrapService.createScrap(scrap);
+
+        // then
+        Assertions.assertEquals(id, scrap.getIdx());
+    }
+
+    @Test
+    void 스크랩삭제() {
+        // given
+        CommunityUser user = new CommunityUser();
+        String nickname = "dr.김";
+        user.setNickname(nickname);
+        user.setIdx(13L);
+        CommunityUser userT = userService.save(user);
+
+        Posting post = new Posting();
+        String content = "안녕하세요";
+        String title = "제목";
+        Long userIdx = userT.getIdx();
+        Posting create_post = postingService.create(userIdx, content, title);
+        Posting save_post = postingService.save(create_post);
+
+        Scrap scrap = Scrap.createScrap(userT.getIdx(), save_post.getIdx());
+        scrapService.createScrap(scrap);
+
+        // when
+        scrapService.deleteScrap(userT.getIdx(), save_post.getIdx());
+
+        // then
+        // deleteScrap은 반환값이 void이기 때문에 생략
+    }
+}
