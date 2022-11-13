2cadc6b98aa4f90b51870b94b3215b3392f7a26c
rosesua318 <rosesua@duksung.ac.kr>
2022-11-13 17:04:08 +0900

    [Fix] 내가 쓴 게시글, 내가 스크랩한 게시글 조회 응답값에 게시글 개수 필드 삭제 (김민서)

--- src/main/java/com/ae/community/dto/response/CheckMyScrapsDto.java
+++ src/main/java/com/ae/community/dto/response/CheckMyScrapsDto.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 @Data
 public class CheckMyScrapsDto {
-    @ApiModelProperty(value = "사용자가 스크랩한 게시글 개수")
-    private Long postCount;
     @ApiModelProperty(value = "사용자가 스크랩한 게시글 리스트")
     private List<PostsListDto> postsLists;
 }
